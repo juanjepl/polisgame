b278255054d6b50ce4ccb075908e205d5e1f28e4
juanjepl <juanjepl@gmail.com>
2010-11-05 13:38:13 +0000

    mas carpetitas aun

--- polisgame/src/utils/PolReader.java
+++ polisgame/src/utils/PolReader.java
@@ -1,8 +1,22 @@
 package utils;
 
-import java.io.*;
+// import java.io.*;
+import game.*;
+
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class PolReader { // Reads .pol files
+	
+	/*public PolReader(){}
+	
+	public Map<String,Sea> readSeas(){
+		Map<String,Sea> seasMap = new HashMap<String,Sea>();
+		return seasMap;
+	}
+		
+	*/	
 	
 	
 	
