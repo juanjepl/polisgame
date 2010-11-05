f9c9970e744076af60bda44c4b37992b6f1b0ed1
juanjepl@gmail.com <juanjepl@gmail.com@8617efda-6465-c61e-1f95-cb14ae943b12>
2010-11-05 22:56:32 +0000

    mas clases para rellenar...

---
+++ trunk/polisgame/src/game/EndRoundManager.java
@@ -0,0 +1,39 @@
+package game;
+
+public class EndRoundManager {
+
+	public EndRoundManager(){}
+	
+		public void checkSiegues(){
+			//TODO
+		}
+		
+		public void checkProjects(){
+			//TODO
+		}
+	
+		public void checkFeeding(){
+			//TODO
+		}
+		
+		public void checkGrowning(){
+			//TODO
+		}
+		
+		public void checkMegalopolis(){
+			//TODO
+		}
+		
+		public void checkGoodsAdjust(){
+			//TODO
+		}
+		
+		public void checkPhoros(){
+			//TODO
+		}
+		
+		public void initializeNextRound(){
+			//TODO
+		}
+}
+
