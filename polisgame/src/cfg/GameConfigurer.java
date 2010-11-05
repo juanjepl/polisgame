303846f5be6262a17c161faa762cd73528f48d97
juanjepl <juanjepl@gmail.com>
2010-11-05 21:09:49 +0000

    comentarios, creacion de archivo de configuracion(vacío), interfaz para interfaces de usuario, y pequeños cambios

---
+++ polisgame/src/cfg/GameConfigurer.java
@@ -0,0 +1,7 @@
+package cfg;
+
+public class GameConfigurer {
+
+	//TODO
+	
+}
---
+++ polisgame/src/ui/IUserInterface.java
@@ -0,0 +1,10 @@
+package ui;
+
+import java.util.List;
+
+public interface IUserInterface {
+
+	public List<String> requestPlayerNames();
+	//TODO
+	
+}
