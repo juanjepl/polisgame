c6b80946b13b66f30408409852905001454b41c4
juanjepl <juanjepl@gmail.com>
2010-11-06 17:25:47 +0000

    añado esquemas de otros paquetes, mas carpetas y algunas correcciones NOTA: ahora mismo no es ejecutable el programa

--- polisgame/src/ui/GraphicalModeUi.java
+++ polisgame/src/ui/GraphicalModeUi.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class GraphicalModeUi implements IUserInterface{
+	
+	public GraphicalModeUi(){}
+	
 	public List<String> requestPlayerNames(){
 		List<String> players = new ArrayList<String>();// FIXME only provisional to not generate a interface error
 		
