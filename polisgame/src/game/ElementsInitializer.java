c31f1e95788112447a41f0b06e93213fbec47736
Samuel Navas <sanapo@gmail.com>
2010-11-15 23:58:30 +0000

    comentario de ElementsInitializer del market antiguo borrado

--- polisgame/src/game/ElementsInitializer.java
+++ polisgame/src/game/ElementsInitializer.java
@@ -29,46 +29,29 @@ public class ElementsInitializer {
 		//// Readings and initialization for markets		
 		Map<String,Market> marketsMap = polisFilesReader.readMarkets();
 		
-		/*
-		obsolete version: like reference for the moment. 
-		marketsMap.put("Iliria",iliria);
-		marketsMap.put("Tracia",tracia);
-		marketsMap.put("EuxinosPontos",euxinosPontos);
-		marketsMap.put("ImperioPersa",imperioPersa);
-		marketsMap.put("Egipto",egipto);
-		
-		
-		Market iliria = new Market("Iliria","Iliria") ;
-		Market tracia = new Market("Tracia","Tracia") ;
-		Market euxinosPontos = new Market("EuxinosPontos","EuxinosPontos") ;
-		Market imperioPersa = new Market("ImperioPersa","ImperioPersa") ;
-		Market egipto = new Market("Egipto","Egipto") ;
-		*/
-		
-		
-	//// Readings and initialization for projects
+		//// Readings and initialization for projects
 		List<Project> gameProjects = polisFilesReader.readProjects();
 
-	//// Readings and initialization for polis. (must be the last of positions to be initialized. Projects also needs to be initialized before.)
+		//// Readings and initialization for polis. (must be the last of positions to be initialized. Projects also needs to be initialized before.)
 		Map<String,Polis> polisMap = polisFilesReader.readPolis(territoriesMap,seasMap,gameProjects);
 		
-	//// Readings and initialization for game events
+		//// Readings and initialization for game events
 		List<List<GameEvent>> gameEventsList = polisFilesReader.readGameEvents();
 		
-	//// Initialization of the round
+		//// Initialization of the round
 		Round theRound = new Round();
 		
-	//// Initialization of the market chart
+		//// Initialization of the market chart
 		MarketChart theMarketChart = new MarketChart();
 				
-	//// Initialization of the game players
+		//// Initialization of the game players
 		TextModeUi userInterface = new TextModeUi(); //TODO Creates a user interface object -> in the future, choose graphical/text mode in cfg. 
 		List<String> players = 	userInterface.requestPlayerNames(); // A method who request Game users their names
 		
 		Player sparta = new Player(players.get(0)); // When Game requests players names, the method returns a list with 2 names, this 2 lines takes this information to create Players instances.
 		Player athens = new Player(players.get(1));
 		
-	//// Initialization of the Game object, who contains all elements of the game initialized before.
+		//// Initialization of the Game object, who contains all elements of the game initialized before.
 		Game polisGame = new Game(sparta,athens,territoriesMap,seasMap,tradeDocksMap,marketsMap,polisMap,gameProjects,gameEventsList,theRound,theMarketChart);
 		return polisGame;
 	}
