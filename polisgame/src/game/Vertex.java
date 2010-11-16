ec6abb0eb50b75dd0f9b8c4339db36abd6463aae
Samuel Navas <sanapo@gmail.com>
2010-11-16 15:12:34 +0000

    cambiar vertex por lista de Vertex

--- polisgame/src/game/Vertex.java
+++ polisgame/src/game/Vertex.java
@@ -1,19 +1,20 @@
 package game;
 
+import java.util.List;
 import java.util.Set;
 
 /** This class represents a vertex, used like base component by Graph class */
 public class Vertex<P extends Position> {
     
-    private P vertexReference;
+    private List<P> vertexReference;
     private Set<Vertex<P>> adjacents; // The group of all adjacents vertex to this vertex
     
-    public Vertex( P vertexReference, Set<Vertex<P>> adjacents){
+    public Vertex( List<P> vertexReference, Set<Vertex<P>> adjacents){
         this.vertexReference = vertexReference;
         this.adjacents = adjacents;
     }
     
-    public P getVertexReference(){
+    public List<P> getVertexReference(){
         return vertexReference;
     }
     
