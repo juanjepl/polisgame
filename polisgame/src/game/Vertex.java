489ddaff7f22328d4c7ccd6493d72de62be6d368
juanjepl <juanjepl@gmail.com>
2010-11-16 17:13:05 +0000

    detalle para la implementación de vértice

--- polisgame/src/game/Vertex.java
+++ polisgame/src/game/Vertex.java
@@ -6,15 +6,15 @@ import java.util.Set;
 /** This class represents a vertex, used like base component by Graph class */
 public class Vertex{
     
-    private List<String> vertexReference;
+    private List<?> vertexReference;
     private Set<Vertex> adjacents; // The group of all adjacents vertex to this vertex
     
-    public Vertex( List<String> vertexReference, Set<Vertex> adjacents){
+    public Vertex( List<Position> vertexReference, Set<Vertex> adjacents){
         this.vertexReference = vertexReference;
         this.adjacents = adjacents;
     }
     
-    public List<String> getVertexReference(){
+    public List<?> getVertexReference(){
         return vertexReference;
     }
     
