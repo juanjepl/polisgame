131e45267bb3cb2772bd99727fa6be8153cc81c6
juanjepl <juanjepl@gmail.com>
2010-11-16 17:29:39 +0000

    Tipo del atributo cambiado a Position finalmente

--- polisgame/src/game/Vertex.java
+++ polisgame/src/game/Vertex.java
@@ -1,20 +1,19 @@
 package game;
 
-import java.util.List;
 import java.util.Set;
 
 /** This class represents a vertex, used like base component by Graph class */
 public class Vertex{
     
-    private List<?> vertexReference;
+	private Position vertexReference;
     private Set<Vertex> adjacents; // The group of all adjacents vertex to this vertex
     
-    public Vertex( List<Position> vertexReference, Set<Vertex> adjacents){
+    public Vertex(Position vertexReference, Set<Vertex> adjacents){
         this.vertexReference = vertexReference;
         this.adjacents = adjacents;
     }
     
-    public List<?> getVertexReference(){
+    public Position getVertexReference(){
         return vertexReference;
     }
     
