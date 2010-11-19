1b73fe63cef48e6a779f24bdf7dd98a257d31c2f
joseajcarmona <joseajcarmona@8617efda-6465-c61e-1f95-cb14ae943b12>
2010-11-19 12:20:41 +0000

    Añadida a la clase RandomCollections comprobaciones de los parámetros de entrada del método getRandomSublist.

--- trunk/polisgame/src/utils/RandomCollections.java
+++ trunk/polisgame/src/utils/RandomCollections.java
@@ -6,8 +6,20 @@ import java.util.Random;
 
 public class RandomCollections<T>
 {
+	/**
+	 * Obtains a list sublist with random elements.
+	 * @param list -- List from which get the sublist.
+	 * @param elementCount -- Number of random elements to get.
+	 * @return The sublist.
+	 * @exception NullPointerException -- if list or elementCount is null.
+	 * @exception IllegalArgumentException -- if list size if less than elementCount.
+	 */
 	public List<T> getRandomSublist(List<T> list, Integer elementCount)
 	{
+		if (list == null) throw new NullPointerException("'list' can not be null");
+		if (elementCount == null) throw new NullPointerException("'elementCount' can not be null");
+		if (list.size() < elementCount) throw new IllegalArgumentException("'list' has less elements than 'elementCount'");
+		
 		List<T> listCopy = new LinkedList<T>(list);
 		List<T> sublist = new ArrayList<T>(elementCount);	
 		
