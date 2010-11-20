d4269341e7d9278dc8d6e980c8070d71bc4238ed
juanjepl@gmail.com <juanjepl@gmail.com@8617efda-6465-c61e-1f95-cb14ae943b12>
2010-11-20 20:28:35 +0000

    Modificaciones en acciones, A partir de ahora, todas las acciones devolverán un Boolean indicando si se ha podido realizar o no. Metodo createHoplite( ) Implementado.

--- trunk/polisgame/src/game/MilitaryAction.java
+++ trunk/polisgame/src/game/MilitaryAction.java
@@ -10,32 +10,32 @@ public class MilitaryAction extends Action{
 	
 	
 	/** To move an Hoplite or Hoplites group, to other position */
-	public void moveHoplite(Player player, Position initialPosition, Position finalPosition, Integer numberOfUnits, Boolean multiMovement){
-		
+	public Boolean moveHoplite(Player player, Position initialPosition, Position finalPosition, Integer numberOfUnits, Boolean multiMovement){
+		Boolean success = false;
 		//TODO
-		
+		return success;
 	}
 	
 	/** To move a Trirreme or Trirremes group, to other sea */
-	public void moveTrirreme(Player player, Sea initialSea, Sea finalSea, Integer numberOfUnits, Boolean multiMovement){
-		
+	public Boolean moveTrirreme(Player player, Sea initialSea, Sea finalSea, Integer numberOfUnits, Boolean multiMovement){
+		Boolean success = false;
 		//TODO
-		
+		return success;
 	}
 	
 	/** Method to manage polis's siegues */
-	public void siegePolis(Player player,Position initialPosition, Polis siegedPolis ){
-		
+	public Boolean siegePolis(Player player,Position initialPosition, Polis siegedPolis ){
+		Boolean success = false;
 		//TODO
 		// not needs numberOfUnits (u CAN or CANNOT siegue a polis)
-		
+		return success;
 	}
 	
 	/** Method to manage takings in the territories */
-	public void plunderTerritory(Player player){
-		
+	public Boolean plunderTerritory(Player player){
+		Boolean success = false;
 		//TODO
-		
+		return success;
 	}
 
 	public String getActionType() {
--- trunk/polisgame/src/game/PoliticAction.java
+++ trunk/polisgame/src/game/PoliticAction.java
@@ -4,37 +4,35 @@ package game;
 public class PoliticAction extends Action{
 
 	private final String actionType = "politicAction"; // A constant to check action's type
-	
-	
+
 	public PoliticAction(){}
 	
-	
 	/** Method to manage when a player starts a project into a polis */
-	public void startProject(Player player, Project project, Polis polis){
-		
+	public Boolean startProject(Player player, Project project, Polis polis){
+		Boolean success = false;
 		//TODO
-		
+		return success;
 	}
 	
 	/** Method to manage when a tradeboat swaps resources into a market */
-	public void trade(Player player, TradeBoat tradeboat, Market market){
-		
+	public Boolean trade(Player player, TradeBoat tradeboat, Market market){
+		Boolean success = false;
 		//TODO needs also a parameter who says what transaction to do
-		
+		return success;
 	}
 	
 	/** Method to manage proxenus's movements */
-	public void moveProxenus(Player player, Polis destination){
-		
+	public Boolean moveProxenus(Player player, Polis destination){
+		Boolean success = false;
 		//TODO
-		
+		return success;
 	}
 	
 	/** Method to manage when a proxenus causes a civilwar in a neutral or enemy polis */
-	public void civilWar(Player player, Polis designatedPolis){
-		
+	public Boolean civilWar(Player player, Polis designatedPolis){
+		Boolean success = false;
 		//TODO
-		
+		return success;
 	}
 
 	public String getActionType() {
