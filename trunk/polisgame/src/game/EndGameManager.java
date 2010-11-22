38e11dbe55f74876eabfdd64db8b3b19b78abd69
elgranmanolo@gmail.com <elgranmanolo@gmail.com@8617efda-6465-c61e-1f95-cb14ae943b12>
2010-11-22 22:20:22 +0000

    Codigo para el checkpolis

--- trunk/polisgame/src/game/EndGameManager.java
+++ trunk/polisgame/src/game/EndGameManager.java
@@ -1,49 +1,56 @@
 package game;
 
-/** This class contains methods to manage the final of the game, when all rounds have finished */
+/**
+ * This class contains methods to manage the final of the game, when all rounds
+ * have finished
+ */
 public class EndGameManager {
 
-	private Player winner = null; // Player who wins the game. (null while game has not a winner)
-	
-	
-	public EndGameManager(){}
-	
-	
-	/** This method checks if any player has lost his capital */
-	public Boolean checkCapitals(){ 
-		Boolean lostCapital = false;
-		
-		//TODO this method checks no one player has lost his capital (game over)
-		
-		return lostCapital;
+	private Player winner = null; // Player who wins the game. (null while game
+									// has not a winner)
+
+	public EndGameManager() {
 	}
-	
+
+	/** This method checks if any player has lost his capital */
+	public Boolean checkCapitals(Player polisGamer) {
+
+		// TODO this method checks no one player has lost his capital (game
+		// over)
+		return !polisGamer.getPlayerPolis().contains(polisGamer.getCapital());
+	}
+
 	/** This method checks if any player has lost all prestige points */
-	public Boolean checkNoPrestige(Player playerGamer){
-		
-		
-		//TODO this method checks no one player has lost all his prestige (game over)
-		
+	public Boolean checkNoPrestige(Player playerGamer) {
+
+		// TODO this method checks no one player has lost all his prestige (game
+		// over)
+
 		return playerGamer.getPrestige() == 0;
 	}
-	
-	/** This method checks the final "score" to decide a winner (following the game rules) */
-	public void checkStandardEndGame(){
-		
-		//TODO this method tell us the winner of the game (is called standard because in the future, can be more types of endings)
-		
+
+	/**
+	 * This method checks the final "score" to decide a winner (following the
+	 * game rules)
+	 */
+	public void checkStandardEndGame() {
+
+		// TODO this method tell us the winner of the game (is called standard
+		// because in the future, can be more types of endings)
+
 	}
-	
-	public Player getWinner(){ // FIXME is it necessary?
-		//TODO
+
+	public Player getWinner() { // FIXME is it necessary?
+		// TODO
 		return winner;
 	}
-	
-	/** This method ends the game */ //FIXME is also it necessary?
-	public void endTheGame(){
-		
-		//TODO
-		
+
+	/** This method ends the game */
+	// FIXME is also it necessary?
+	public void endTheGame() {
+
+		// TODO
+
 	}
-	
+
 }
