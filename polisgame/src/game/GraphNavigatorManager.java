9c38a46417e74676c834f7abfe3bef560f95296d
Samuel Navas <sanapo@gmail.com>
2010-11-23 17:57:02 +0000

    metodo existsWay() funcional

--- polisgame/src/game/GraphNavigatorManager.java
+++ polisgame/src/game/GraphNavigatorManager.java
@@ -32,19 +32,66 @@ public class GraphNavigatorManager {
 		Set<Vertex<? extends Position>> searchInGraph = graph.getGraph().keySet();
 		
 		Vertex<? extends Position> v = null;
+		Vertex<? extends Position> v2 = null;
 		for(Vertex<? extends Position> vertex: searchInGraph)
 		{
 
 			if(vertex.getVertexReference().getSysName().equals(p1.getSysName()))
 			{
 				v = vertex;
+			}else if(vertex.getVertexReference().getSysName().equals(p2.getSysName()))
+			{
+				v2 = vertex;
 			}
 		}
-		
+		//Get adjacents of v vertex
 		List<Vertex<? extends Position>> initialPositionAdjacents = graph.getGraph().get(v);
+		
+		
 		Boolean exists = false;
+		List<Vertex<? extends Position>> visited = new ArrayList<Vertex<? extends Position>>();
+		List<Vertex<? extends Position>> queue = new ArrayList<Vertex<? extends Position>>();
 		
 		for(Vertex<? extends Position> vertex: initialPositionAdjacents)
+		{
+			
+			if(!vertex.getVertexReference().getLockForAPlayer(player))
+			{
+					queue.add(vertex);
+			}else
+			{
+				visited.add(vertex);
+			}
+			
+		}
+		
+		visited.add(v);
+		
+		while(!queue.isEmpty() && !exists)
+		{
+			//extract first vertex
+			Vertex<? extends Position> element = queue.remove(0);
+			//get adjacents of element and check if exists in visited
+			List<Vertex<? extends Position>> adjacents = graph.getGraph().get(element);
+			
+			for(Vertex<? extends Position> vertex: adjacents)
+			{
+				//check if vertex isn't locked and isn't visited
+				if(!vertex.getVertexReference().getLockForAPlayer(player) && !visited.contains(vertex))
+				{
+						//add vertex to queue
+						queue.add(vertex);
+				}
+				
+			}
+			
+			//check if p2 vertex exists in queue
+			if(queue.contains(v2))
+			{
+				exists = true;
+			}
+		}
+		/*for(Vertex<? extends Position> vertex: initialPositionAdjacents)
 		{
 
 			List<Vertex<? extends Position>> possibleCandidates = possibleCandidates(graph.getGraph().get(vertex), player);
@@ -52,42 +99,9 @@ public class GraphNavigatorManager {
 			
 			exists = (exists || findCandidate(possibleCandidates, p2));  
 			
-		}
+		}*/
 	
 		return exists;
-	}
-	
-	private static List<Vertex<? extends Position>> possibleCandidates(List<Vertex<? extends Position>> vertices, Player player)
-	{
-		List<Vertex<? extends Position>> newCandidates = new ArrayList<Vertex<? extends Position>>();
-		
-		for(Vertex<? extends Position> vertex: vertices)
-		{
-			
-			if(!vertex.getVertexReference().getLockForAPlayer(player))
-			{
-					newCandidates.add(vertex);
-			}
-			
-		}
-		
-		
-		return newCandidates;
-	}
-	
-	private static Boolean findCandidate(List<Vertex<? extends Position>> possibleCandidates, Position p2)
-	{
-		Boolean candidate = false;
-		
-		for(Vertex<? extends Position> vertex: possibleCandidates)
-		{
-			if(vertex.getVertexReference().getSysName().equals(p2.getSysName()))
-			{
-				candidate = true;
-			}
-		}
-		
-		return candidate;
 	}
 
 }
