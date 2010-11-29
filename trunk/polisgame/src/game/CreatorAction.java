ebc521c8944fbf50b8130bf121a00cc4005132b7
hafurg@gmail.com <hafurg@gmail.com@8617efda-6465-c61e-1f95-cb14ae943b12>
2010-11-29 11:12:24 +0000

    Ya estan los metodos para crear el proxeno y el barco mercante

--- trunk/polisgame/src/game/CreatorAction.java
+++ trunk/polisgame/src/game/CreatorAction.java
@@ -88,19 +88,50 @@ public class CreatorAction extends Action{
 	/** Creates a Trade Boat unit in polis's trade dock */
 	public Boolean createTradeBoat(Player owner, Polis polis, Round round){
 		Boolean success = false;
-		//TODO
+		if(success){
+
+			if(owner.getWood()>=1 && owner.getSilver()<1){
+				owner.setWood(owner.getWood() - 1);
+			}else if(owner.getWood()<1 && owner.getSilver()>=1){
+				owner.setSilver(owner.getSilver() - 1);
+			}else{
+
+				String paid = TextModeUi.requestPaidMethod( "tradeBoat" );
+
+				if(paid.equals( "Wood" ) ){
+					owner.setWood(owner.getWood() - 1);
+				}else if(paid.equals( "Silver" ) ){
+					owner.setSilver(owner.getSilver() - 1);
+				}else{
+					success = false;
+				}
+			}
+			polis.setActualPopulation(polis.getActualPopulation() - 1);	
+			owner.getPlayerTradeDock().addUnit(new TradeBoat(owner));
+			
+			}else{
+				//Do nothing,success=false;
+			}
 		return success;
 	}
 	
 	/** Creates a Proxenus unit in selected polis */
 	public Boolean createProxenus(Player owner, Polis polis){
 		Boolean success = false;
-		//TODO
+		success = AvailableActionsManager.checkCreateProxenusAction (owner , polis);
+		
+		if(success){
+		
+		owner.setSilver ( owner.getSilver() - 4);
+		polis.setActualPopulation ( polis.getActualPopulation() - 1);
+		polis.getUnits().add(new Proxenus(owner));
+		}else{
+			//do nothing->return success=false
+		}
 		return success;
 	}
 
 	public String getActionType() {
 		return actionType;
 	}
-	
 }
