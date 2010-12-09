04c03da4587b0aa92340a3b0507dd9da75ca1116
marsesancha <marsesancha@8617efda-6465-c61e-1f95-cb14ae943b12>
2010-12-09 11:56:23 +0000

    He actualizado las pruebas unitarias TestGenericReader y TestDirectoryReader, las primeras van "bien", auqnue sigue tragándose el caso de que se meta una string que no es una ruta. Esto hay que solucionarlo. El segundo va fatal, todo da errores y no salen las listas que deberían salir. He actualizado también los ficheros de pruebas guardandolos en .pol

--- trunk/polisgame/src/tests/TestGenericDirectoryReader.java
+++ trunk/polisgame/src/tests/TestGenericDirectoryReader.java
@@ -1,5 +1,8 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,48 +18,38 @@ import utils.GenericReader;
 
 public class TestGenericDirectoryReader {
 	
-	List<List<String>> list = new ArrayList<List<String>>();
-	Map<String,List<String>> map = new HashMap<String,List<String>>(); 
-
+	List<List<String>> list;
+	Map<String,List<String>> map;
 	@Before
 	public void setup() {
+		list = new ArrayList<List<String>>();
+		map = new HashMap<String,List<String>>(); 
 
 	}
 
 	@Test//(expected=FileNotFoundException.class)
 	public void testgetDirectoryFilesVoid(){
 		list = GenericDirectoryReader.getDirectoryFiles("src/tests/TestFiles/TestVoidDirectory");
-		TestUtiles.printListToList(list);
+		assertEquals(list,null);
+		//TestUtiles.printListToList(list);
 	}
 	@Test
 	public void testgetDirectoryFiles() {
 		list = GenericDirectoryReader.getDirectoryFiles("src/tests/TestFiles/TestDirectory");
-		TestUtiles.printListToList(list);
+		assertTrue(list.contains("TestList.pol"));
+		assertTrue(list.contains("TestListNull.pol"));
+		assertTrue(list.contains("TestListSharp.pol"));
+		//TestUtiles.printListToList(list);
 	}
 
-	
-//	public void printList(List<List<String>> list){
-//		for(List<String> l: list){
-//			for(String s: l){
-//				System.out.println(s);
-//			}
-//		}
-//	}
+
 	
 	@Test
 	public void testgetDirectoryFilesMap() {
 		map = GenericDirectoryReader.getDirectoryFilesMap("src/tests/TestFiles/TestDirectory");
-		TestUtiles.printMapList(map);
+		//TestUtiles.printMapList(map);
 	}
 	
-//	public void printMap(Map<String,List<String>> map){
-//		for(String s: map.keySet()){
-//			for(List<String> list: map.values()){
-//				for(String s2: list){
-//					System.out.println(s);
-//				}
-//			}
-//		}
-//	}
+
 
 }
