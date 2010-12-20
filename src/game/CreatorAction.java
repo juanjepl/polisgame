0054122fe5346204732d20cef53f2a99e26b5c9d
juanjepl <juanjepl@gmail.com>
2010-12-20 20:32:39 +0000

    BRANCH COMMIT : empezada refactorizacion sobre acciones

--- src/game/CreatorAction.java
+++ src/game/CreatorAction.java
@@ -1,53 +1,10 @@
 package game;
 
-import ui.TextModeUi;
-
 /** This class contains the methods for execute creation actions in the game */
 public class CreatorAction extends GameAction{
-	
-	private final String actionType = "creatorAction"; // A constant to check action's type
-	
-	public CreatorAction(){}
-	
-	/** Creates an Hoplite unit in polis's parent territory */
-	public Boolean createHoplite(Player owner, Polis polis, Round round){
-		Boolean success = false;
-		success = AvailableActionsManager.checkCreateHopliteAction(owner, polis, round);
-		
-		if(success){
-			
-			// if only have metal to pay, uses it, same with silver. if both -> player chooses.
-			if(owner.getMetal() >= 1 && owner.getSilver() < 1){
-				owner.setMetal(owner.getMetal() - 1);
-			}else if(owner.getMetal() < 1 && owner.getSilver() >= 1){
-				owner.setSilver(owner.getSilver() - 1);
-			}else{
-				
-				String paid = TextModeUi.requestPaidMethod("hoplite");
-				
-				if(paid.equals("Metal")){
-					owner.setMetal(owner.getMetal() - 1);
-				}else if(paid.equals("Silver")){
-					owner.setSilver(owner.getSilver() - 1);
-				}else{
-					success = false; // Invalid resource ( wrong value of paid )
-				}	
-			}
-			
-			polis.setActualPopulation(polis.getActualPopulation() - 1);
-			Hoplite hop = new Hoplite(owner,polis.getPolisParentTerritory());
-			polis.getPolisParentTerritory().addUnit(hop);
-			owner.addUnit(hop);
-			
-			TextModeUi.showMessage("Hoplite created at "+polis.getPolisParentTerritory().getName());
-			
-		}else{
-			// Do nothing -> success returns false
-		}
 
-		return success; // returns an "advice" to know if the action has been done or not.
-	}
-	
+	public CreatorAction(){}
+
 	/** Creates a Trirreme unit in polis's sea */
 	public Boolean createTrirreme(Player owner, Polis polis, Round round){
 		Boolean success = false;
@@ -170,7 +127,3 @@ public class CreatorAction extends GameAction{
 		return success;
 	}
 
-	public String getActionType() {
-		return actionType;
-	}
-}
