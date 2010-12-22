4376347a3ecd9fd366d910b04d3882cd7ffb1a27
sanapo@gmail.com <sanapo@gmail.com@8617efda-6465-c61e-1f95-cb14ae943b12>
2010-12-22 16:17:00 +0000

    BRANCH COMMIT: refactorizacion de grafos en Game

--- branches/polis_refactorizado_post-it5/src/game/MoveProxenusAction.java
+++ branches/polis_refactorizado_post-it5/src/game/MoveProxenusAction.java
@@ -2,10 +2,39 @@ package game;
 
 public class MoveProxenusAction extends MilitaryAction{
 
-	public MoveProxenusAction(){
+	private Player player;
+	private Polis destiny;
+	private Integer amountToPayForWay;
+
+	public MoveProxenusAction(Player player, Polis destiny, Integer amountToPayForWay){
 		super();
+		if(player == null){
+			throw new IllegalArgumentException("Player musn't be null");
+		}
+		
+		this.player = player;
+		this.destiny = destiny;
+		this.amountToPayForWay = amountToPayForWay;
+		
+		getPlayer().setSilver(getPlayer().getSilver() - getAmountToPayForWay());
 	
-		//TODO
+		getPlayer().getPlayerProxenus().setPosition(getDestiny());
 		
 	}
+	
+	public Player getPlayer()
+	{
+		return player;
+	}
+	
+	public Integer getAmountToPayForWay()
+	{
+		return amountToPayForWay;
+	}
+	
+	public Polis getDestiny()
+	{
+		return destiny;
+	}
+	
 }
