787a1f6d92fb8fc31c8410b8c642867a6436f594
juanjepl@gmail.com <juanjepl@gmail.com@8617efda-6465-c61e-1f95-cb14ae943b12>
2010-12-28 11:45:42 +0000

    BRANCH COMMIT: refactorizado EndRoundCheckProjects

--- branches/polis_refactorizado_post-it5/src/game/EndRoundCheckProjects.java
+++ branches/polis_refactorizado_post-it5/src/game/EndRoundCheckProjects.java
@@ -1,5 +1,42 @@
 package game;
 
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * This class manages, in round ends
+ * the payment to a player for finished
+ * projects in his polis
+ */
 public class EndRoundCheckProjects {
 
+	public EndRoundCheckProjects(Player p){
+		if(p == null){
+			throw new IllegalArgumentException("Invalid parameter for EndRoundCheckProjects constructor, cannot be null");
+		}
+		
+		List<Project> projectsToPay = new ArrayList<Project>();
+		
+		// Take the projects to be finished
+		for(Polis po: p.getPlayerPolis()){
+			for(Project proj: po.getProjects()){
+				if((proj.getUsed()) == true && (!(proj.getFinished()))){
+					projectsToPay.add(proj);
+					break; // only inner loop, only one project can be finished per round in one polis
+				}
+			}
+		}
+		
+		// Pays to the player, the reward for project finished
+		for(Project pj : projectsToPay){
+			if(pj.getSysName().equals("phidiasArtist")){ // Only athensPlayer can do this project in his capital
+				p.setPrestige(p.getPrestige() + Math.round(p.getCapital().getActualPopulation() / 2));
+				pj.setFinished(true);
+				
+			}else{
+				p.setPrestige(p.getPrestige() + pj.getPrestige());
+				pj.setFinished(true);
+			}
+		}
+	}	
 }
