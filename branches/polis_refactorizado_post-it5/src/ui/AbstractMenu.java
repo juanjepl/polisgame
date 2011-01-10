2209d7b5fce3cfbb9e970344eb98752ab62fee79
joseajcarmona <joseajcarmona@8617efda-6465-c61e-1f95-cb14ae943b12>
2011-01-10 12:21:02 +0000

    Corregida la clase AbstractMenu con un nuevo atributo (gameTexts).

--- branches/polis_refactorizado_post-it5/src/ui/AbstractMenu.java
+++ branches/polis_refactorizado_post-it5/src/ui/AbstractMenu.java
@@ -4,7 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractMenu implements IMenu {
+	private Map<String, String> gameTexts;
+	
 	public AbstractMenu(Map<String, String> gameTexts) {
+		if (gameTexts == null) throw new NullPointerException("gameText cannot be null");
+		this.gameTexts = gameTexts;
+	}
+	
+	public Map<String, String> getGameTexts() {
+		return gameTexts;
 	}
 	
 	public abstract String getHeaderMessage();
