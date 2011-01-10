7e00bb235971c098eeab199a842b678d51e1caa4
joseajcarmona <joseajcarmona@8617efda-6465-c61e-1f95-cb14ae943b12>
2011-01-10 11:33:01 +0000

    Corregido el AbstractMenu.

--- branches/polis_refactorizado_post-it5/src/ui/AbstractMenu.java
+++ branches/polis_refactorizado_post-it5/src/ui/AbstractMenu.java
@@ -3,20 +3,18 @@ package ui;
 import java.util.List;
 
 public abstract class AbstractMenu implements IMenu {
-	public abstract String getHeaderMessage() {
-	}
+	public abstract String getHeaderMessage();
+	
+	public abstract List<String> getMenuOptionList();
+	
+	public abstract void execute();
 
-	public abstract List<String> getMenuOptionList() {
+	public Integer requestPlayerChoice() {
+		//TODO
+		return null;
 	}
 	
-	public abstract void execute() {
-	}
-
-	public abstract Integer requestPlayerChoice() {
-	}
-	
-	public abstract IMenu getNextMenu() {
-	}
+	public abstract IMenu getNextMenu();
 	
 	public void convertChoose(Integer choose) {
 		//TODO
