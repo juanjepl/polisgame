0ba6eba4eafff3b1b9d02cf16dd9e01c4ea55d1e
joseajcarmona <joseajcarmona@8617efda-6465-c61e-1f95-cb14ae943b12>
2011-01-11 16:35:45 +0000

    Añadidos los menus (por completar) de Acción Político.

--- branches/polis_refactorizado_post-it5/src/ui/MilitaryActionMenu.java
+++ branches/polis_refactorizado_post-it5/src/ui/MilitaryActionMenu.java
@@ -5,10 +5,10 @@ import java.util.Map;
 import exceptions.PolisGameRunningException;
 import game.Game;
 
-public class PoliticActionMenu extends AbstractMenu {
+public class MilitaryActionMenu extends AbstractMenu {
 	private Game game;
 	
-	public PoliticActionMenu(Map<String, String> gameTexts, List<IMenu> menuList, Game game) {
+	public MilitaryActionMenu(Map<String, String> gameTexts, List<IMenu> menuList, Game game) {
 		super(gameTexts, menuList);
 		if (game == null) throw new IllegalArgumentException("'game' cannot be null");
 		this.game = game;
@@ -16,19 +16,20 @@ public class PoliticActionMenu extends AbstractMenu {
 		List<String> optionList = getMenuOptionsList();
 		
 		// TODO Hay que ver cuales opciones de las siguientes estan disponibles:
-		optionList.add(texts.get("gamePoliticActionMenu_startProjectOpt"));
-		optionList.add(texts.get("gamePoliticActionMenu_tradeOpt"));
-		optionList.add(texts.get("gamePoliticActionMenu_moveProxenusOpt"));
-		optionList.add(texts.get("gamePoliticActionMenu_makeCivilWarOpt"));
+		optionList.add(texts.get("gameMilitaryActionMenu_moveHoplitesOpt"));
+		optionList.add(texts.get("gameMilitaryActionMenu_moveTrirremesOpt"));
+		optionList.add(texts.get("gameMilitaryActionMenu_besiegePolisOpt"));
+		optionList.add(texts.get("gameMilitaryActionMenu_collectionOpt"));
 		optionList.add(texts.get("gamePoliticActionMenu_cancel"));
 	}
 
 	public void execute() {
 		setPlayerChoice(requestPlayerChoice());
+		// TODO
 	}
 
 	public String getHeaderMessage() {
-		return getGameTexts().get("gamePoliticActionMenu_headerMessage");
+		return getGameTexts().get("gameMilitaryActionMenu_headerMessage");
 	}
 
 	public IMenu getNextMenu() {
@@ -37,16 +38,16 @@ public class PoliticActionMenu extends AbstractMenu {
 		// TODO Hay que ver cuales opciones de las siguientes estan disponibles:
 		/*switch (getPlayerChoice()) {
 			case 0:
-				next = new PoliticActionStartProjectMenu(getGameTexts(), getMenuList(), game);
+				next = new MilitaryActionMoveHoplitesMenu(getGameTexts(), getMenuList(), game);
 				break;
 			case 1:
-				next = new PoliticActionTradeMenu(getGameTexts(), getMenuList(), game);
+				next = new MilitaryActionMoveTrirremesMenu(getGameTexts(), getMenuList(), game);
 				break;
 			case 2:
-				next = new PoliticActionMoveProxenusMenu(getGameTexts(), getMenuList(), game);
+				next = new MilitaryActionBesiegePolisMenu(getGameTexts(), getMenuList(), game);
 				break;
 			case 3:
-				next = new PoliticActionMakeCivilWarMenu(getGameTexts(), getMenuList(), game);
+				next = new MilitaryActionCollectionMenu(getGameTexts(), getMenuList(), game);
 				break;
 			case 4:
 				next = getMenuList().get((getMenuList().size()) - 1); // Last element
@@ -57,5 +58,4 @@ public class PoliticActionMenu extends AbstractMenu {
 		
 		return next;
 	}
-
 }
