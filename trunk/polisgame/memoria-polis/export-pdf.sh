9231491b09458b55fcbf56aca1b4c894cb6da202
joseajcarmona <joseajcarmona@8617efda-6465-c61e-1f95-cb14ae943b12>
2011-01-11 19:37:58 +0000

    Borrado totalísimo.

--- trunk/polisgame/memoria-polis/export-pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pdflatex -output-directory=pdf polis.tex 
-pdflatex -output-directory=pdf polis.tex
-rm pdf/polis.aux
-rm pdf/polis.log
-rm pdf/polis.toc
--- trunk/polisgame/src/game/TradeBoat.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-/** Trade boat unit Class */
-public class TradeBoat extends Unit{
-
-	public TradeBoat(Player pl,Position po){ // uses Unit constructor
-		super(pl,po);
-	}
-}
