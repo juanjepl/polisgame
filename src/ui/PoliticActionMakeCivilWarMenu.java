cc5438f8adae265957a5f5a6d6caab424f9fde05
Samuel Navas <sanapo@gmail.com>
2011-01-14 00:28:47 +0000

    PoliticActionMakeCivilWarMenu creado

--- src/ui/PoliticActionMakeCivilWarMenu.java
+++ src/ui/PoliticActionMakeCivilWarMenu.java
@@ -1,22 +1,38 @@
 package ui;
 
+import game.CivilWarAction;
 import game.Game;
+
 import java.util.List;
 import java.util.Map;
 
 public class PoliticActionMakeCivilWarMenu extends AbstractMenu {
-	private Game game;
 	
 	public PoliticActionMakeCivilWarMenu(Map<String, String> gameTexts, List<IMenu> menuList, Game game) {
 		super(gameTexts, menuList);
+		this.setGame(game);
 		if (game == null) throw new IllegalArgumentException("'game' cannot be null");
-		this.game = game;
 		// TODO
 	}
 
 	public void execute() {
+		
+		getMenuOptionsList().add(getGameTexts().get("back"));
+		getMenuOptionsList().add(getGameTexts().get("politicActionMakeCivilWarMenu_doCivilWar"));
+		showMenuContents();
 		setPlayerChoice(requestPlayerChoice());
-		// TODO
+		
+		if(getPlayerChoice().equals("1"))
+		{
+			// Do action
+			CivilWarAction civilWarAction = new CivilWarAction(getGame().getWhoHasTheTurn());
+		
+			// Introduces Action in the actual turn
+			getGame().getRound().getCurrentTurn().addGameAction(civilWarAction);
+		
+			// Finally a notification for the player
+			showActionDoneMessage(getGame().getWhoHasTheTurn().getPlayerProxenus().getPosition().getName());
+		}
 	}
 
 	public String getHeaderMessage() {
@@ -25,11 +41,25 @@ public class PoliticActionMakeCivilWarMenu extends AbstractMenu {
 
 	public IMenu getNextMenu() {
 		IMenu next = null;
-		Integer choice = getPlayerChoice();
-		
-		// TODO
+		switch (getPlayerChoice()) {
+		case 0:
+		// Exit:
+		next = getMenuList().get((getMenuList().size()-1) - 1); // Last element
+		break;
+	
+		default:
+		next = new GameMainMenu(getGameTexts(),getMenuList(), getGame()); //go back to this menu
+		next.setAutoExecutable(false); //FIXME provisional. //action completed
+		break;
+	}
 		
 		return next;
 	}
+	
+	public void showActionDoneMessage(String polisName){
+		System.out.println(" "); // White space
+		System.out.println(getGameTexts().get("PoliticActionCivilWarMenu_civilWarDone")+ " " + polisName);//FIXME
+	}
+	
 
 }
