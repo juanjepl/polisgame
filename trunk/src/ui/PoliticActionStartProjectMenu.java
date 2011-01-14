377a0ff7d856642ea458b58904b3f671bc06e71e
sanapo@gmail.com <sanapo@gmail.com@8617efda-6465-c61e-1f95-cb14ae943b12>
2011-01-14 00:03:18 +0000

    PoliticActionStartProject terminado a falta de añadir un metodo

--- trunk/src/ui/PoliticActionStartProjectMenu.java
+++ trunk/src/ui/PoliticActionStartProjectMenu.java
@@ -1,22 +1,62 @@
 package ui;
 
+import game.AvailableActionsManager;
 import game.Game;
+import game.Polis;
+import game.Project;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PoliticActionStartProjectMenu extends AbstractMenu {
-	private Game game;
+
+	private List<String> availableValuesForRequest;
+	private List<Project> possibleProjects;
 	
 	public PoliticActionStartProjectMenu(Map<String, String> gameTexts, List<IMenu> menuList, Game game) {
 		super(gameTexts, menuList);
+		this.setGame(game);
 		if (game == null) throw new IllegalArgumentException("'game' cannot be null");
-		this.game = game;
-		//TODO: El numero de opciones (proyectos) es variable
+		availableValuesForRequest = new ArrayList<String>();
+		possibleProjects = new ArrayList<Project>();
 	}
 
+	public List<Project> getPossibleProjects()
+	{
+		return possibleProjects;
+	}
+	
 	public void execute() {
-		setPlayerChoice(requestPlayerChoice());
-		// TODO
+		
+		if(getMenuOptionsList().isEmpty())
+		{
+
+			List<String> optionList = getMenuOptionsList();
+			
+			// Opt 0 (Exit):
+			optionList.add(getGameTexts().get("back"));
+			availableValuesForRequest.add("0");
+			
+			Integer index = 1;
+			
+			for(Polis po : getGame().getWhoHasTheTurn().getPlayerPolis()){
+                for(Project proj: getGame().getRound().getProjectsInThisRound()){
+                        if(AvailableActionsManager.checkStartProjectAction(po, proj)){
+                                possibleProjects.add(proj);
+                                optionList.add(proj.getName());
+                                availableValuesForRequest.add(index.toString());
+                                index++;
+                        }
+                }
+        }
+			
+			
+		}
+		
+		showMenuContents();
+		setPlayerChoice(requestPlayerChoice(getAvailableValuesForRequest()));
+		
 	}
 
 	public String getHeaderMessage() {
@@ -25,12 +65,22 @@ public class PoliticActionStartProjectMenu extends AbstractMenu {
 
 	public IMenu getNextMenu() {
 		IMenu next = null;
-		Integer choice = getPlayerChoice();
+		switch (getPlayerChoice()) {
+		case 0:
+			// Exit:
+			next = getMenuList().get((getMenuList().size()-1) - 1); // Last element
+			break;
 		
-		//TODO: El numero de opciones (proyectos) es variable, no se puede poner un switch y ni una estructura fija de IFs
-		// next = getMenuList().get((getMenuList().size()) - 1); // Esto sera para volver al menu anterior
-		
+		default:
+			//create new menu and pass selected project
+			next = new PoliticActionStartProjectSelectPolisMenu(getGameTexts(),getMenuList(), getGame(), getPossibleProjects().get(getPlayerChoice()));
+			break;
+		}
 		return next;
+	}
+	
+	public List<String> getAvailableValuesForRequest(){
+		return availableValuesForRequest;
 	}
 
 }
