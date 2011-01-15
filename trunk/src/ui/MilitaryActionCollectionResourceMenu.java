53c2c4cbd72e4796671f0cf1581d457fc0f6bb61
joseajcarmona <joseajcarmona@8617efda-6465-c61e-1f95-cb14ae943b12>
2011-01-15 15:45:39 +0000

    Submenú Elegir Recurso del Menú de Saqueo de Territorio añadido.

--- trunk/src/ui/MilitaryActionCollectionResourceMenu.java
+++ trunk/src/ui/MilitaryActionCollectionResourceMenu.java
@@ -1,13 +1,11 @@
 package ui;
 
 import exceptions.PolisGameRunningException;
-import game.AvailableActionsManager;
 import game.Game;
-import game.Player;
 import game.Territory;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
@@ -16,6 +14,7 @@ public class MilitaryActionCollectionResourceMenu extends AbstractMenu {
 	private Game game;
 	private List<String> availableValuesForRequest;
 	private Territory territoryToPlunder;
+	private Map<Integer, String> availableResources;
 	
 	public MilitaryActionCollectionResourceMenu(Map<String, String> gameTexts, List<IMenu> menuList, Game game, Territory territoryToPlunder) {
 		super(gameTexts, menuList);
@@ -23,6 +22,7 @@ public class MilitaryActionCollectionResourceMenu extends AbstractMenu {
 		this.game = game;
 		this.territoryToPlunder = territoryToPlunder;
 		availableValuesForRequest = new ArrayList<String>();
+		availableResources = new HashMap<Integer, String>(6);
 	}
 
 	public Game getGame(){
@@ -38,16 +38,21 @@ public class MilitaryActionCollectionResourceMenu extends AbstractMenu {
 			List<String> optList = getMenuOptionsList();
 			Map<String, String> texts = getGameTexts();
 			
-			Player currentPlayer = game.getWhoHasTheTurn();
-			
 			// Opcion volver
 			optList.add(texts.get("back"));
 			availableValuesForRequest.add("0");
-			
-			
+				
 			// Opciones de recursos
 			Map<String, Vector<Integer>> territoryResources = territoryToPlunder.getResources();
-			
+			Integer i = 1;
+			for (String resourceName: territoryResources.keySet()) {
+				
+				// Opcion de recurso
+				optList.add(texts.get(resourceName.toLowerCase()));
+				availableValuesForRequest.add(i.toString());
+				availableResources.put(i, resourceName.toLowerCase());
+				i++;
+			}
 		}
 		
 		showMenuContents();
@@ -59,20 +64,21 @@ public class MilitaryActionCollectionResourceMenu extends AbstractMenu {
 	}
 
 	public String getHeaderMessage() {
-		return getGameTexts().get("gameMilitaryActionMoveTrirremesDestinationMenu_headerMessage");
+		return getGameTexts().get("gameMilitaryActionCollectionResourceMenu_headerMessage");
 	}
 
 	public IMenu getNextMenu() {
-		IMenu next;
-	
+		//IMenu next;
+		IMenu next = null;
+		
 		Integer userChoice = getPlayerChoice();
 		if (userChoice < 0 || userChoice > (availableValuesForRequest.size() - 1)) throw new PolisGameRunningException("Option not available choosen at MilitaryActionMoveTrirremesDestinationMenu");
 		
 		if(userChoice.equals(0)){
 			next = getMenuList().get((getMenuList().size()-1) - 1);
 		} else {
-			Sea choosenDestinationPosition = destinationPositions.get(userChoice - 1);
-			next = new MilitaryActionMoveTrirremesUnitCountMenu(getGameTexts(), getMenuList(), game, originPosition, choosenDestinationPosition);
+			String resourceToPlunder = availableResources.get(userChoice);
+			//next = new MilitaryActionMoveTrirremesUnitCountMenu(getGameTexts(), getMenuList(), game, territoryToPlunder, resourceToPlunder);
 		}
 		
 		return next;
