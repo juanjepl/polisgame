baf2700a9c6757ec3e6ea0720b9acd66cc3be4f6
juanjepl@gmail.com <juanjepl@gmail.com@8617efda-6465-c61e-1f95-cb14ae943b12>
2011-01-17 17:03:17 +0000

    limpiadas lineas que no se utilizaban

--- trunk/src/ui/CreationActionMenu.java
+++ trunk/src/ui/CreationActionMenu.java
@@ -3,7 +3,6 @@ package ui;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import exceptions.PolisGameRunningException;
 import game.AvailableActionsManager;
 import game.Game;
 import game.Player;
