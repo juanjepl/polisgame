cbe64dcfa52a9fb82252686a1e6e888972b1ad43
Samuel Navas <sanapo@gmail.com>
2011-01-17 07:49:49 +0000

    añadido texto en gametexts

---
+++ src/ui/PoliticActionTradeChooseResourcesMenu.java
@@ -0,0 +1,84 @@
+package ui;
+
+import game.Game;
+import game.Market;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class PoliticActionTradeChooseResourcesMenu extends AbstractMenu{
+	
+	private Market market;
+	private List<String> availableValuesForRequest;
+	
+	public PoliticActionTradeChooseResourcesMenu(Map<String, String> gameTexts, List<IMenu> menuList,Game game, Market market)
+	{
+		super(gameTexts, menuList);
+		this.setGame(game);
+	if (game == null) throw new IllegalArgumentException("'game' cannot be null");
+		
+		availableValuesForRequest = new ArrayList<String>();
+
+	}
+
+	public Market getMarket()
+	{
+		return market;
+	}
+	
+	public void execute() {
+		
+		if(getMenuOptionsList().isEmpty())
+		{
+			List<String> optionList = getMenuOptionsList();
+			
+			// Opt 0 (Exit):
+			optionList.add(getGameTexts().get("back"));
+			availableValuesForRequest.add("0");
+			
+			Integer index = 1;
+			
+			for(String resource1: getMarket().getResources().keySet())
+			{
+				for(String resource2: getMarket().getResources().get(resource1).keySet())
+				{
+					
+					optionList.add(resource2);
+					availableValuesForRequest.add(index.toString());
+					index++;
+				}
+			}
+		}
+		
+		showMenuContents();
+		setPlayerChoice(requestPlayerChoice(getAvailableValuesForRequest()));
+		// TODO
+	}
+
+	public String getHeaderMessage() {
+		return getGameTexts().get("politicActionTradeChooseResourcesMenu_headerMessage");
+	}
+
+	public IMenu getNextMenu() {
+		IMenu next = null;
+		
+		switch (getPlayerChoice()) {
+		case 0:
+			// Exit:
+			next = getMenuList().get((getMenuList().size()-1) - 1); // Last element
+			break;
+		default:
+			
+			Market market = getGame().getGameMarkets().get(getMenuList().get(getPlayerChoice()));
+			String resource2 = getMenuOptionsList().get(getPlayerChoice());
+			next = new PoliticActionTradeChooseResourceNeededMenu(getGameTexts(), getMenuList(), getGame(), market, resource2);
+			break;
+		}
+		return next;
+	}
+	
+	public List<String> getAvailableValuesForRequest(){
+		return availableValuesForRequest;
+	}
+}
